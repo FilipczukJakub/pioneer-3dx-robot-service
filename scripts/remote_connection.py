8a18c94168701aa5f005c38b7d6c4b20ff5a1f54
vboxuser <filipczukjakub@gmail.com>
2024-03-24 14:09:07 +0100

    small_changes

--- scripts/remote_connection.py
+++ scripts/remote_connection.py
@@ -11,11 +11,9 @@ import threading
 import json
 import time
 
-main_server_running = False
-stop = False
+global_stop = False
 msg = Twist()
-controlled_move_thread = None
-avoid_obstacle_thread = None
+ping_thread = threading.Thread
 
 def toTwist(jsonString):
     local_msg = Twist()
@@ -29,54 +27,55 @@ def toTwist(jsonString):
     return local_msg
 
 async def echo(websocket):
-    global stop
     global msg
-    global controlled_move_thread
-    global avoid_obstacle_thread
-    try:
-        async for message in websocket:
-            print(message)
-            if(not message.startswith("message:")):
-                jsonString = json.loads(message)
-                msg = toTwist(jsonString)
-            else:
-                avoid_obstacle_thread.start()
-    except Exception as e:
-        print('blad servera ' + e)
-
-def pose_callback(pose):
-    sonars = PointCloud()
+    global ping_thread
+    ping_thread = threading.Thread(target=ping_handler,args=(websocket,))
+    ping_thread.start()
     while True:
-        sonars = pose
-        print(sonars)
-        time.sleep(5)
+        message = await websocket.recv()
+        print(message)
+        jsonString = json.loads(message)
+        msg = toTwist(jsonString)
 
-def obstacle_avoidance():
-    print('subscribed to sonars')
-    sub = rospy.Subscriber("/RosAria/sonar",PointCloud,callback=pose_callback)
-
-async def main_server(ip):
-    global stop
+async def main_server(ip,stop):
     async with websockets.serve(echo,ip,8765):
         print("server is listening on " + ip + ":8765")
         await stop
+    print('server stopped')
+
+def ping_handler(websocket):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(ping(websocket))
+    loop.close()
+
+async def ping(websocket):
+    global global_stop
+    while not global_stop:
+        try:
+            await websocket.send('ping')
+            print('ping')
+            time.sleep(2)
+        except Exception as e:
+            break
 
 def controlled_move():
     global msg
+    global global_stop
     pub = rospy.Publisher("/RosAria/cmd_vel", Twist, queue_size=10)
     print('move service started')
-    while True:
-        # print('move vector: ' + str(msg))        
+    while not global_stop:
         pub.publish(msg)
         time.sleep(0.1)
 
 def broadcast_server(ip):
     global msg
+    global global_stop
     s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('0.0.0.0',12345))
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     print('broadcast started')
-    while(True):
+    while not global_stop:
         data,adres= s.recvfrom(1024)
         message = data.decode('ascii')
         if(message == 'ip_request'):
@@ -87,21 +86,21 @@ def broadcast_server(ip):
             msg = toTwist(0)
 
 async def start_controlling_service():
-    global stop
-    global controlled_move_thread
-    global avoid_obstacle_thread
+    global global_stop
     ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]    
     broadcast_thread = threading.Thread(target=broadcast_server,args=(ip,))  
     controlled_move_thread = threading.Thread(target=controlled_move,args=())
-    avoid_obstacle_thread = threading.Thread(target=obstacle_avoidance,args=())
     broadcast_thread.start()
     controlled_move_thread.start()
     loop = asyncio.get_event_loop()
     stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-    await main_server(ip)
+    loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+    await main_server(ip,stop)
+    global_stop = True
+    print('finish threads')
 
 if __name__ == '__main__':
     rospy.init_node('remote_listener')
     asyncio.run(start_controlling_service())
     rospy.spin()
+
