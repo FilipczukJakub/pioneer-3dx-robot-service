d2e049ed3c08b40d0377762789374306fe228377
f_jakub <filipczukjakub@gmail.com>
2024-10-29 15:33:07 +0100

    camera_feed

--- scripts/remote_connection.py
+++ scripts/remote_connection.py
@@ -3,6 +3,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import PointCloud
 from websockets.server import serve
+from picamera import PiCamera
 import websockets
 import asyncio
 import socket
@@ -11,6 +12,8 @@ import threading
 import json
 import time
 import os
+import io
+from PIL import Image
 
 global_stop = False
 msg = Twist()
@@ -43,6 +46,39 @@ async def main_server(ip,stop):
         print("server is listening on " + str(ip) + ":8765")
         await stop
     print('server stopped')
+
+async def camera_server(ip,stop):
+    async with websockets.serve(send_camera_feed,'0.0.0.0',8766):
+        print("camera is listening on " + str(ip) + ":8766")
+        await stop
+    print('camera stopped')
+
+async def send_camera_feed(websocket, path):
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    try:
+        while True:
+            # Capture the image from the camera as a byte stream
+            stream = io.BytesIO()
+            camera.capture(stream, format='jpeg')
+            stream.seek(0)
+            
+            # Optionally, you can resize or modify the image using PIL
+            image = Image.open(stream)
+            
+            # Save the image into bytes
+            img_byte_array = io.BytesIO()
+            image.save(img_byte_array, format='JPEG')
+            img_data = img_byte_array.getvalue()
+            
+            # Send the image data to the client over WebSocket
+            await websocket.send(img_data)
+            
+            # Pause before capturing the next frame
+            await asyncio.sleep(1)
+    except websockets.ConnectionClosed:
+        print("Client disconnected")
+
 
 def ping_handler(websocket):
     loop = asyncio.new_event_loop()
@@ -99,7 +135,10 @@ async def start_controlling_service():
     loop = asyncio.get_event_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
-    await main_server(ip,stop)
+    await asyncio.gather(
+        main_server(ip,stop),
+        camera_server(ip,stop)
+    )
     global_stop = True
     print('finish threads')
 
